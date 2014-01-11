eb1ae301c387027ec9edd1baa8100a34b1caee29
Ravit Khurana <ravit.khurana@gmail.com>
2014-01-11 17:20:55 +0530

    Nothing major

--- SubNote.py
+++ SubNote.py
@@ -1,8 +1,22 @@
-
+import traceback
+import subprocess
 import sublime
 import sublime_plugin
-from evernote.api.client import EvernoteClient
 
 class TestCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		print("Hello SubNote!")
+		self.caller()
+		print("Bye SubNote")
+
+	def caller(self):
+		command = ["python", "./evernote_test/evernote_test.py"]
+		try:
+			err_file = open("error.log", 'w')
+			output = subprocess.check_output(command, universal_newlines=True, stderr=err_file)
+		except:
+			traceback.print_exc(file=sys.stdout)
+			output = "Error Occured."
+
+		print(output)
+		
--- evernote_test/evernote_test.py
+++ evernote_test/evernote_test.py
@@ -1,12 +1,17 @@
+import sys
+import os
 from evernote.api.client import EvernoteClient
 import evernote.edam.type.ttypes as Types
 
+# TODO: Get rid of __file__ by using modules
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def run():
-    # Creating instance of EvernoteClient
-    f = open('dev_token', 'r')
+    # Creating instance of EvernoteClient    
+    f = open(BASE_PATH + '/dev_token', 'r')
     dev_token = f.read()
     client = EvernoteClient(token=dev_token)
+    f.close()
 
     # Getting instance of userStore.
     # Getting instance of user - This contains the accounts details of the user
@@ -22,4 +27,5 @@ def run():
         print "-" + n.name
 
 if __name__ == "__main__":
+    print("Running on python version" + sys.version)
     run()
